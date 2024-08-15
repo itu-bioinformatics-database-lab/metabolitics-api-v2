aca9c5b2b0c2e0d8f370bcba5c5c10b806e3c8af
ahmetenisguven <ahmetenisguven@gmail.com>
2024-08-15 16:37:47 +0300

    P-values are changed with q-values for the Pathway Enrichment method

--- src/app/pe.py
+++ src/app/pe.py
@@ -1,5 +1,6 @@
 from .base import MetaboliticsBase
 from scipy.stats import hypergeom
+from statsmodels.stats.multitest import multipletests
 
 class PathwayEnrichment():
     def __init__(self, concentration_table):
@@ -20,13 +21,21 @@ class PathwayEnrichment():
         all_reactions = self.base.get_reaction_names()
         data_metabolites = self.fold_changes.keys()
         n = len(data_metabolites)
+
+        p_values = []
         for reaction in all_reactions:
             reaction_metabolites = self.base.get_metabolites_by_reaction(reaction)
             K = len(reaction_metabolites)
             common_metabolites = [value for value in data_metabolites if value in reaction_metabolites]
             k = len(common_metabolites)
             pval = hypergeom.sf(k-1, N, K, n)
-            reactions_scores[reaction] = pval
+            #reactions_scores[reaction] = pval
+            p_values.append(pval)
+
+        q_values = multipletests(p_values, method="fdr_bh")[1]
+
+        for reaction, q_value in zip(all_reactions, q_values):
+            reactions_scores[reaction] = q_value
         return reactions_scores
     
     def score_pathways(self):
@@ -40,13 +49,22 @@ class PathwayEnrichment():
         all_pathways = self.base.get_pathway_names()
         data_metabolites = self.fold_changes.keys()
         n = len(data_metabolites)
+
+        p_values = []
         for pathway in all_pathways:
             pathway_metabolites = self.base.get_metabolites_by_pathway(pathway)
             K = len(pathway_metabolites)
             common_metabolites = [value for value in data_metabolites if value in pathway_metabolites]
             k = len(common_metabolites)
             pval = hypergeom.sf(k-1, N, K, n)
-            pathways_scores[pathway] = pval
+            #pathways_scores[pathway] = pval
+            p_values.append(pval)
+        
+        q_values = multipletests(p_values, method="fdr_bh")[1]
+        
+        for reaction, q_value in zip(all_pathways, q_values):
+            pathways_scores[reaction] = q_value
+        
         return pathways_scores
     
     def display_pathway_scores(self):
